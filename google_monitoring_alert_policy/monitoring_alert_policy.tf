0e3f034e10f5ae3044472d64f74f875689b8e036
sidd000000 <96447204+sidd000000@users.noreply.github.com>
2022-01-25 15:02:59 -0500

    update

--- google_monitoring_alert_policy/monitoring_alert_policy.tf
+++ google_monitoring_alert_policy/monitoring_alert_policy.tf
@@ -16,15 +16,13 @@ resource "google_monitoring_alert_policy" "alert_policy" {
       condition_matched_log               = conditions.value.condition_matched_log 
 
       
-
-      
       dynamic "condition_absent" {
          
         for_each = lookup(conditions.value, "condition_absent", [])
 
         content {
-          duration         = condition_absent.value.duration
-          filter = lookup(condition_absent.value, "filter", null)
+          duration  = condition_absent.value.duration
+          filter    = lookup(condition_absent.value, "filter", null)
 
             dynamic "aggregations" {
 
@@ -42,7 +40,7 @@ resource "google_monitoring_alert_policy" "alert_policy" {
                 for_each = lookup(condition_absent.value, "trigger", [])
                 content {
                     percent   = lookup(trigger.value, "percent", null)
-                    count      = lookup(trigger.value, "count", null)
+                    count     = lookup(trigger.value, "count", null)
                 }
             }
   
@@ -66,18 +64,15 @@ resource "google_monitoring_alert_policy" "alert_policy" {
                              cross_series_reducer    = lookup(denominator_aggregations.value, "cross_series_reducer", null)
 
                         }
-
                     }
 
                     dynamic "trigger" {
                         for_each = lookup(condition_threshold.value, "trigger", [])
                         content {
-                             percent      = lookup(trigger.value, "percent", null)
-                             count         = lookup(trigger.value, "count", null)
-                             
+                             percent       = lookup(trigger.value, "percent", null)
+                             count         = lookup(trigger.value, "count", null)             
 
                         }
-
                     }
 
                     dynamic "aggregations" {
@@ -85,12 +80,10 @@ resource "google_monitoring_alert_policy" "alert_policy" {
                         content {
                              per_series_aligner      = lookup(aggregations.value, "per_series_aligner", null)
                              group_by_fields         = lookup(aggregations.value, "group_by_fields", null)
-                             alignment_period         = lookup(aggregations.value, "alignment_period", null)
-                             cross_series_reducer         = lookup(aggregations.value, "cross_series_reducer", null)
-                             
+                             alignment_period        = lookup(aggregations.value, "alignment_period", null)
+                             cross_series_reducer    = lookup(aggregations.value, "cross_series_reducer", null)   
 
                         }
-
                     }
 
                 dynamic "condition_matched_log" {
@@ -101,17 +94,11 @@ resource "google_monitoring_alert_policy" "alert_policy" {
                              
 
                         }
-                }
+                    }
 
                 }
 
             }
-
-
+         }
     }
-  }
-
-
-
-  
 }
