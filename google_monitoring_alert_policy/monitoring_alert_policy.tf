d290b76e02d1d53abf5660923fa800eb58e0adef
sidd000000 <96447204+sidd000000@users.noreply.github.com>
2022-01-25 14:04:55 -0500

    update

--- google_monitoring_alert_policy/monitoring_alert_policy.tf
+++ google_monitoring_alert_policy/monitoring_alert_policy.tf
@@ -13,7 +13,7 @@ resource "google_monitoring_alert_policy" "alert_policy" {
 
       
 
-      # conditions_absent could be capped to 1 block
+      
       dynamic "condition_absent" {
          
         for_each = lookup(conditions.value, "condition_absent", [])
@@ -43,9 +43,67 @@ resource "google_monitoring_alert_policy" "alert_policy" {
             }
   
         }
-      }
+      }   
+            dynamic "condition_threshold" {
+                for_each = lookup(conditions.value, "condition_threshold", [])
+                content {
+                    threshold_value      = lookup(condition_threshold.value, "threshold_value", null)
+                    denominator_filter   = lookup(condition_threshold.value, "denominator_filter", null)
+                    duration             = conditions.value.duration
+                    comparison           = conditions.value.comparison
+                    filter               = lookup(conditions.value, "filter", null)
 
-      
+                    dynamic "denominator_aggregations" {
+                        for_each = lookup(condition_threshold.value, "denominator_aggregations", [])
+                        content {
+                             per_series_aligner      = lookup(denominator_aggregations.value, "per_series_aligner", null)
+                             group_by_fields         = lookup(denominator_aggregations.value, "group_by_fields", null)
+                             alignment_period        = lookup(denominator_aggregations.value, "alignment_period", null)
+                             cross_series_reducer    = lookup(denominator_aggregations.value, "cross_series_reducer", null)
+
+                        }
+
+                    }
+
+                    dynamic "trigger" {
+                        for_each = lookup(condition_threshold.value, "trigger", [])
+                        content {
+                             percent      = lookup(trigger.value, "percent", null)
+                             count         = lookup(trigger.value, "count", null)
+                             
+
+                        }
+
+                    }
+
+                    dynamic "aggregations" {
+                        for_each = lookup(condition_threshold.value, "aggregations", [])
+                        content {
+                             per_series_aligner      = lookup(aggregations.value, "per_series_aligner", null)
+                             group_by_fields         = lookup(aggregations.value, "group_by_fields", null)
+                             alignment_period         = lookup(aggregations.value, "alignment_period", null)
+                             cross_series_reducer         = lookup(aggregations.value, "cross_series_reducer", null)
+                             
+
+                        }
+
+                    }
+
+                dynamic "condition_matched_log" {
+                    for_each = lookup(conditions.value, "condition_matched_log", [])
+                        content {
+                             filter                   = lookup(condition_matched_log.value, "filter", null)
+                             label_extractors         = lookup(condition_matched_log.value, "label_extractors", null)
+                             
+
+                        }
+                }
+
+                }
+
+            }
+
+
     }
   }
 
